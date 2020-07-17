735438830653259cc7c6f857f08b9ddb59625370
Takkuya <takuyagabriel@gmail.com>
2020-07-17 20:44:12 -0300

    mexendo um pouco no back-end

---
+++ database.js
@@ -0,0 +1,9 @@
+const users = [
+    {id: "1", nome: "carlos", idade : 12},
+    {id: "2", nome : "Maria", idade : 19},
+    {id: "3", nome: "Jorisvinho", idade : 32},
+    {id: "4", nome: "Paulinho", idade:28},
+    {id: "5", nome: "Cristoverson", idade:2020}
+]
+
+module.exports = users
